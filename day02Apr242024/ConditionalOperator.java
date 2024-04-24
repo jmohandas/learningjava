0a67ca4072fc141f4a1213f5f970c8464afbcb99
Jithu Mohandas <jithu.mohandas@scout24.com>
2024-04-24 07:00:39 +0200

    Ternary operator code

---
+++ day02Apr242024/ConditionalOperator.java
@@ -0,0 +1,7 @@
+class ConditionalOperator {
+    public static void main(String args[]) {
+        int first = 10;
+        int result = first == 10 ? 5 : 20;
+        System.out.println("Result : "+result);
+    }
+}
---
+++ day02Apr242024/ShiftOperator.java
@@ -0,0 +1,7 @@
+class ShiftOperator {
+    public static void main(String args[]) {
+        int a = 12;
+        int leftShiftResult = a << 1;
+        System.out.println(a + " Left shift result = "+leftShiftResult);
+    }
+}
